3f671b9b670794ad5887874e24be3125ab38a36b
Recyclingbottle <hoholeo382@gmail.com>
2022-11-16 14:06:45 +0900

    ViewList 에 Json 파싱 내용 띄우기

--- app/src/main/java/kr/ac/kumoh/s20180468/android_volley/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180468/android_volley/MainActivity.kt
@@ -1,6 +1,8 @@
 package kr.ac.kumoh.s20180468.android_volley
 
+import android.R
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
@@ -8,31 +10,53 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kr.ac.kumoh.s20180468.android_volley.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var queue: RequestQueue
-    override fun onCreate(savedInstanceState: Bundle?){
+    private val movies = ArrayList<String>()
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         queue = Volley.newRequestQueue(application)
-        binding.btnConnect.setOnClickListener{
+        binding.btnConnect.setOnClickListener {
             val url = "https://yts.torrentbay.to/api/v2/list_movies.json?sort=rating&limit=30"
             val request = JsonObjectRequest(
                 Request.Method.GET,
                 url,
                 null,
                 {
-                    Toast.makeText(application, it.toString(), Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(application, it.toString(), Toast.LENGTH_LONG).show()
+                    movies.clear()
+                    parseJson(it)
+//                    Toast.makeText(application,
+//                        movies.toString()
+//                            .replace(",", "\n")
+//                            .replace("[\\[\\]]".toRegex(), ""),
+//                        Toast.LENGTH_LONG).show()
+                    binding.listMovies.adapter = ArrayAdapter<String>(this,
+                        R.layout.simple_list_item_1,
+                        movies)
                 },
                 {
-                    Toast.makeText(application, it.toString(), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(application, it.toString(), Toast.LENGTH_LONG).show()
                 }
             )
-            queue.add(request)
 
+            queue.add(request)
         }
     }
+
+    private fun parseJson(obj: JSONObject) {
+        val items = obj.getJSONObject("data").getJSONArray("movies")
+        for (i in 0 until items.length()) {
+            val item: JSONObject = items[i] as JSONObject
+            val title = item.getString("title_long")
+            movies.add(title)
+        }
+    }
+
 }
